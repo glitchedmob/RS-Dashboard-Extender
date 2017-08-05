58a42d441bfb8cebc6a2aceb575c37e8105cf829
Levi Zitting <levizitting5663@gmail.com>
2017-08-05 08:25:58 -0500

    More comments

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -5,6 +5,9 @@ import Controller from './controller';
 const store = new Store();
 const view = new View();
 
+// Since most page data is pulled in via AJAX,
+// our app needs to delay it's start to account for that
+// Eventually I want to find out how to perform this using events
 setTimeout(() => {
 	const controller = new Controller(store, view);
 }, 1000)
